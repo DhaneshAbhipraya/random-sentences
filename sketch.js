ece5984a9a53570df1ccc49091d08b5fb9c8e460
DhaneshAbhipraya <dhanesh.abhipraya@gmail.com>
2022-06-04 18:35:23 +0700

    add more words

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let texts = [];
-let randomNouns = ["book","chair","plant","broom","mop","door","table","pot","pane","banner","stair","pit","person"];
+let randomNouns = ["book","chair","plant","broom","mop","door","table","pot","pane","banner","stair","pit","person","elephant","pig","bird","cat","dog","cow","horse","sheep","snake","fish","lion","tiger","bear","zebra","giraffe","ant","bee","butterfly","camel","chicken","dolphin","duck","eagle","fish","goat","gorilla","hippo","kangaroo","lobster","monkey","mouse","penguin","pig","rabbit","rat","snail","snake","spider","turtle","whale","zebra","head","tribe","tank","computer","egg","hat"];
 let randomVerbs = ["test","remind","fail","blame","spend","enclose","top","advocate","contribute","smell","destroy"];
 let randomNames = ["josh","susan","johnie","amandus","viliam","maksimilian","laila","guendoleu","marine","jobeth","asklepiades","eduardo","sabbas","fulbert","julij","božena","agaue","erez","ekundayo","joeri","louis","guanting","líadain","joe","ælfþryð","heracleitus","dejan","sang","niko","hilarion"]
 let randomSentences;
@@ -17,6 +17,7 @@ function setup() {
       "a "+getRand(randomNouns)+" that is "+getRand(randomVerbs)+"ed by a "+getRand(randomNouns),
       "someone "+getRand(randomVerbs)+"s a "+getRand(randomNouns),
       "I ate a "+getRand(randomNouns)+" because "+getRand(randomNames)+" told me",
+      "the "+getRand(randomNouns)+" became the official symbol of our company because of "+getRand(randomNames),
     ];
     let rand=getRand(randomSentences);
     rand=rand.replace(/eed/g,"ed");
