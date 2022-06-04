560e14f2159646b701ef933479c7af2a007b952b
DhaneshAbhipraya <dhanesh.abhipraya@gmail.com>
2022-06-04 18:44:22 +0700

    split nouns and rephrase sentences

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 let texts = [];
-let randomNouns = ["book","chair","plant","broom","mop","door","table","pot","pane","banner","stair","pit","person","elephant","pig","bird","cat","dog","cow","horse","sheep","snake","fish","lion","tiger","bear","zebra","giraffe","ant","bee","butterfly","camel","chicken","dolphin","duck","eagle","fish","goat","gorilla","hippo","kangaroo","lobster","monkey","mouse","penguin","pig","rabbit","rat","snail","snake","spider","turtle","whale","zebra","head","tribe","tank","computer","egg","hat"];
+let randomAnimateNouns = ["person","elephant","pig","bird","cat","dog","cow","horse","sheep","snake","fish","lion","tiger","bear","zebra","giraffe","ant","bee","butterfly","camel","chicken","dolphin","duck","eagle","fish","goat","gorilla","hippo","kangaroo","lobster","monkey","mouse","penguin","pig","rabbit","rat","snail","snake","spider","turtle","whale","zebra"];
+let randomInanimateNouns = ["book","chair","plant","broom","mop","door","table","pot","pane","banner","stair","pit","head","tribe","tank","computer","egg","hat"];
+let randomConsumableNouns = ["apple","banana","orange","pear","peach","grape","strawberry","watermelon","mango","pineapple","coconut","kiwi","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato","carrot","onion","potato","broccoli","cabbage","spinach","lettuce","cauliflower","celery","celery","cucumber","tomato"];
+let randomNouns = randomAnimateNouns.concat(randomInanimateNouns,randomConsumableNouns);
 let randomVerbs = ["test","remind","fail","blame","spend","enclose","top","advocate","contribute","smell","destroy"];
 let randomNames = ["josh","susan","johnie","amandus","viliam","maksimilian","laila","guendoleu","marine","jobeth","asklepiades","eduardo","sabbas","fulbert","julij","božena","agaue","erez","ekundayo","joeri","louis","guanting","líadain","joe","ælfþryð","heracleitus","dejan","sang","niko","hilarion"]
 let randomSentences;
@@ -12,11 +15,11 @@ function setup() {
   });
   for (let i = 0; i < 100; i++) {
     randomSentences = [
-      "a "+getRand(randomNouns)+" that "+getRand(randomVerbs)+"s",
-      "a "+getRand(randomNouns)+" that "+getRand(randomVerbs)+"s for "+round(random(2,20))+" "+getRand(randomNouns)+"s",
-      "a "+getRand(randomNouns)+" that is "+getRand(randomVerbs)+"ed by a "+getRand(randomNouns),
-      "someone "+getRand(randomVerbs)+"s a "+getRand(randomNouns),
-      "I ate a "+getRand(randomNouns)+" because "+getRand(randomNames)+" told me",
+      "a "+getRand(randomAnimateNouns)+" that "+getRand(randomVerbs)+"s",
+      "a "+getRand(randomAnimateNouns)+" that "+getRand(randomVerbs)+"s for "+round(random(2,20))+" "+getRand(randomConsumableNouns)+"s",
+      "a "+getRand(randomAnimateNouns)+" that is "+getRand(randomVerbs)+"ed by a "+getRand(randomAnimateNouns),
+      "someone "+getRand(randomVerbs)+"s a "+getRand(randomAnimateNouns),
+      "I ate a "+getRand(randomConsumableNouns)+" because "+getRand(randomNames)+" told me to",
       "the "+getRand(randomNouns)+" became the official symbol of our company because of "+getRand(randomNames),
     ];
     let rand=getRand(randomSentences);
