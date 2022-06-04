b2fac559a6071c1d3a4a030be92fa56e69b3bda7
DhaneshAbhipraya <dhanesh.abhipraya@gmail.com>
2022-06-04 18:15:08 +0700

    update

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let texts = [];
 let randomNouns = ["book","chair","plant","broom","mop","door","table","pot","pane","banner","stair","pit","person"];
 let randomVerbs = ["test","remind","fail","blame","spend","enclose","top","advocate","contribute","smell","destroy"];
+let randomNames = ["josh","susan","johnie","amandus","viliam","maksimilian","laila","guendoleu","marine","jobeth","asklepiades","eduardo","sabbas","fulbert","julij","božena","agaue","erez","ekundayo","joeri","louis","guanting","líadain","joe","ælfþryð","heracleitus","dejan","sang","niko","hilarion"]
 let randomSentences;
 
 function setup() {
@@ -10,7 +11,13 @@ function setup() {
     location.reload();
   });
   for (let i = 0; i < 100; i++) {
-    randomSentences = ["a "+getRand(randomNouns)+" that "+getRand(randomVerbs)+"s","a "+getRand(randomNouns)+" that "+getRand(randomVerbs)+"s for "+round(random(2,20))+" "+getRand(randomNouns)+"s","a "+getRand(randomNouns)+" that is "+getRand(randomVerbs)+"ed by a "+getRand(randomNouns),"someone "+getRand(randomVerbs)+"s a "+getRand(randomNouns)];
+    randomSentences = [
+      "a "+getRand(randomNouns)+" that "+getRand(randomVerbs)+"s",
+      "a "+getRand(randomNouns)+" that "+getRand(randomVerbs)+"s for "+round(random(2,20))+" "+getRand(randomNouns)+"s",
+      "a "+getRand(randomNouns)+" that is "+getRand(randomVerbs)+"ed by a "+getRand(randomNouns),
+      "someone "+getRand(randomVerbs)+"s a "+getRand(randomNouns),
+      "I ate a"+getRand(randomNouns)+" because "+getRand(randomNames),
+    ];
     let rand=getRand(randomSentences);
     rand=rand.replace(/eed/g,"ed");
     createP((i+1)+':<br>'+rand).style("font-size","30px")
