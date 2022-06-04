6f65a3e5984a80097a95a0e1c089c8cb527a5a7e
DhaneshAbhipraya <dhanesh.abhipraya@gmail.com>
2022-06-04 18:17:14 +0700

    update

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup() {
       "a "+getRand(randomNouns)+" that "+getRand(randomVerbs)+"s for "+round(random(2,20))+" "+getRand(randomNouns)+"s",
       "a "+getRand(randomNouns)+" that is "+getRand(randomVerbs)+"ed by a "+getRand(randomNouns),
       "someone "+getRand(randomVerbs)+"s a "+getRand(randomNouns),
-      "I ate a"+getRand(randomNouns)+" because "+getRand(randomNames),
+      "I ate a "+getRand(randomNouns)+" because "+getRand(randomNames)+" told me",
     ];
     let rand=getRand(randomSentences);
     rand=rand.replace(/eed/g,"ed");
